a220c296b429576f966a5d5a1b2bdd797a94f48a
XiaoSaurus <xiaosaurus@github.com>
2026-03-31 23:04:56 +0800

    config: 小程序 apiBase 设为本机局域网 IP 10.16.247.27

--- miniprogram/app.js
+++ miniprogram/app.js
@@ -3,7 +3,7 @@ App({
   globalData: {
     // 后端 API 地址 - 开发时用本机 IP，上线后换成服务器地址
     // 注意：微信小程序不支持 localhost，需要用本机局域网 IP
-    apiBase: 'http://192.168.1.100:8080',
+    apiBase: 'http://10.16.247.27:8080',
     history: []
   },
 
